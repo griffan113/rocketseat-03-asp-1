70559d3ea2a15d585e3d9d40a32a47cdd392fe05
Eliel <elielmichel@gmail.com>
2024-12-19 08:47:29 -0300

    feat: health check controller

---
+++ Controllers/HealthCheckController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rkst03ASP1.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HealthCheckController : ControllerBase {
+    [HttpGet]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    public IActionResult Index() {
+        return Ok("OK");
+    }
+}
